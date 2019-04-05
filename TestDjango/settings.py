4276a332e8d0c810ed9c8a48eb3169478916863c
dlytyvn <dlytvyn@softserveinc.com>
2019-04-05 18:25:37 +0300

    venv 2

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': {}
 }
 
 
